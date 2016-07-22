a70b549d026e71edc7b1f80fb8a34544f6706b66
Craig Jeffrey <Craig-J@users.noreply.github.com>
2016-07-22 20:42:24 +0100

    Removing formspree redirect attempt 3.

--- js/formspree.js
+++ js/formspree.js
@@ -1,34 +1,26 @@
-var $contactForm = $('#contact_form');
 $(document).ready(function() {
-    $contactForm.submit(function(e) {
-        e.preventDefault();
-        var $submit = $('input:submit', $contactForm);
-        var defaultSubmitText = $submit.val();
-
+    $('#contact_form').validate({
+        messages: { },
+        submitHandler: function(form) {
         $.ajax({
             url: '//formspree.io/craigjeffrey3@gmail.com',
             method: 'POST',
-            data: $(this).serialize(),
+            data: $(form).serialize(),
             dataType: 'json',
             beforeSend: function() {
                 $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-                $submit.attr('disabled', true).val('Sending message…');
             },
             success: function(data) {
                 $contactForm.append('<div class="alert alert--success">Message sent!</div>');
-                $submit.val('Message sent!');
                 setTimeout(function() {
                     $('.alert--success').remove();
-                    $submit.attr('disabled', false).val(defaultSubmitText);
                 }, 5000);
             },
             error: function(err) {
                 $contactForm.find('.alert--loading').hide();
                 $contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
-                $submit.val('Oops, there was an error.');
                 setTimeout(function() {
                     $('.alert--error').remove();
-                    $submit.attr('disabled', false).val(defaultSubmitText);
                 }, 5000);
             }
         });
